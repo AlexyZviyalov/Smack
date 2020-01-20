1b2873cd8e59d3bbb960b6060074c86479d243b6
ALIAKSEI ZAUYALAU <i6828042@icloud.com>
2020-01-20 22:21:50 +0300

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by ALIAKSEI ZAUYALAU on 1/20/20.
+//  Copyright © 2020 ALIAKSEI ZAUYALAU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
